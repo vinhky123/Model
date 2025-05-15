f59342ea0e2e40b61dfd5310fe89573493e119ee
Vĩnh Kỳyy <89082148+vinhky123@users.noreply.github.com>
2025-05-15 14:35:11 +0700

    Update TimeStar.py

--- models/TimeStar.py
+++ models/TimeStar.py
@@ -6,49 +6,6 @@ from layers.Embed import DataEmbedding_inverted, PositionalEmbedding
 import numpy as np
 
 
-class STAR(nn.Module):
-    def __init__(self, d_series, d_core):
-        super(STAR, self).__init__()
-        """
-        STar Aggregate-Redistribute Module
-        """
-
-        self.gen1 = nn.Linear(d_series, d_series)
-        self.gen2 = nn.Linear(d_series, d_core)
-        self.gen3 = nn.Linear(d_series + d_core, d_series)
-        self.gen4 = nn.Linear(d_series, d_series)
-
-    def forward(self, input, *args, **kwargs):
-        batch_size, channels, d_series = input.shape
-
-        # set FFN
-        combined_mean = F.gelu(self.gen1(input))
-        combined_mean = self.gen2(combined_mean)
-
-        # stochastic pooling
-        if self.training:
-            ratio = F.softmax(combined_mean, dim=1)
-            ratio = ratio.permute(0, 2, 1)
-            ratio = ratio.reshape(-1, channels)
-            indices = torch.multinomial(ratio, 1)
-            indices = indices.view(batch_size, -1, 1).permute(0, 2, 1)
-            combined_mean = torch.gather(combined_mean, 1, indices)
-            combined_mean = combined_mean.repeat(1, channels, 1)
-        else:
-            weight = F.softmax(combined_mean, dim=1)
-            combined_mean = torch.sum(
-                combined_mean * weight, dim=1, keepdim=True
-            ).repeat(1, channels, 1)
-
-        # mlp fusion
-        combined_mean_cat = torch.cat([input, combined_mean], -1)
-        combined_mean_cat = F.gelu(self.gen3(combined_mean_cat))
-        combined_mean_cat = self.gen4(combined_mean_cat)
-        output = combined_mean_cat
-
-        return output, None
-
-
 class STAR_patch(nn.Module):
     def __init__(self, d_series, d_core):
         super(STAR_patch, self).__init__()
@@ -245,7 +202,16 @@ class Model(nn.Module):
         self.encoder = Encoder(
             [
                 EncoderLayer(
-                    STAR(configs.d_model, configs.d_core),
+                    AttentionLayer(
+                        FullAttention(
+                            False,
+                            configs.factor,
+                            attention_dropout=configs.dropout,
+                            output_attention=False,
+                        ),
+                        configs.d_model,
+                        configs.n_heads,
+                    ),
                     STAR_patch(configs.d_model, configs.d_core),
                     configs.d_model,
                     configs.d_ff,
