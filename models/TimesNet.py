42f1f09a7d63cc723e31eae3ad6496ed51ecf44e
Vĩnh Kỳyy <89082148+vinhky123@users.noreply.github.com>
2025-05-27 16:16:43 +0700

    Update TimesNet.py

--- models/TimesNet.py
+++ models/TimesNet.py
@@ -120,7 +120,7 @@ class Model(nn.Module):
         x_enc /= stdev
 
         # embedding
-        enc_out = self.enc_embedding(x_enc, x_mark_enc)  # [B,T,C]
+        enc_out = self.enc_embedding(x_enc)  # [B,T,C]
         # TimesNet
         for i in range(self.layer):
             enc_out = self.layer_norm(self.model[i](enc_out))
