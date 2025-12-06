d1ea8fc17ca55dc1336c51e48d1987433974fa3f
Vĩnh Kỳyy <89082148+vinhky123@users.noreply.github.com>
2025-12-06 22:36:17 +0700

    some features

--- exp/exp_basic.py
+++ exp/exp_basic.py
@@ -3,8 +3,8 @@ import torch
 from models import Autoformer, Transformer, TimesNet, Nonstationary_Transformer, DLinear, FEDformer, \
     Informer, LightTS, Reformer, ETSformer, Pyraformer, PatchTST, MICN, Crossformer, FiLM, iTransformer, \
     Koopa, TiDE, FreTS, TimeMixer, TSMixer, SegRNN, MambaSimple, TemporalFusionTransformer, SCINet, PAttn, TimeXer, \
-    WPMixer, MultiPatchFormer, KANAD, MSGNet, TimeFilter, Sundial, TimeMoE, Chronos, Moirai, TiRex,\
-    TimesFM, Toto, Chronos2
+    WPMixer, MultiPatchFormer, KANAD, MSGNet, TimeFilter, Sundial, TimeMoE, Moirai, TiRex,\
+    TimesFM, Toto
 
 
 class Exp_Basic(object):
@@ -45,12 +45,10 @@ class Exp_Basic(object):
             'TimeFilter': TimeFilter,
             'Sundial': Sundial,
             'TimeMoE': TimeMoE,
-            'Chronos': Chronos,
             'Moirai': Moirai,
             'TiRex': TiRex,
             'TimesFM': TimesFM,
             'Toto': Toto,
-            'Chronos2': Chronos2
         }
         if args.model == 'Mamba':
             print('Please make sure you have successfully installed mamba_ssm')
