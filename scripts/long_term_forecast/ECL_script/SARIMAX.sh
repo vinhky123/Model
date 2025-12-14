7300b9dbe0e03687550ea0394b7940ae2e69b515
Vĩnh Kỳyy <89082148+vinhky123@users.noreply.github.com>
2025-12-14 16:15:30 +0700

    update

---
+++ scripts/long_term_forecast/ECL_script/SARIMAX.sh
@@ -0,0 +1,86 @@
+model_name=SARIMAX
+
+# ECL 96
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id ECL_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
+
+# ECL 192
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id ECL_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
+
+# ECL 336
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id ECL_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
+
+# ECL 720
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id ECL_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
+
---
+++ scripts/long_term_forecast/Weather_script/ARIMA.sh
@@ -0,0 +1,86 @@
+model_name=ARIMA
+
+# Weather 96
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --batch_size 32 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
+
+# Weather 192
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --batch_size 32 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
+
+# Weather 336
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --batch_size 32 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
+
+# Weather 720
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --batch_size 32 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
+
